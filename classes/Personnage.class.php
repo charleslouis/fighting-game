918f3fc2775da23488ef696fd7484629798e12d7
kartonnade <clouis.allizard@gmail.com>
2013-07-25 20:04:48 +0200

    second phase of fighting game is working

--- classes/Personnage.class.php
+++ classes/Personnage.class.php
@@ -1,25 +1,24 @@
 <?php
 class Personnage
 {
-  private $_id;
-  private $_degats;
-  private $_nom;
+  protected $atout,
+            $degats,     
+            $id,
+            $nom,
+            $timeEndormi,
+            $type;
   
   const CEST_MOI = 1;
   const PERSONNAGE_TUE = 2;
   const PERSONNAGE_FRAPPE = 3;
+  const PERSONNAGE_ENSORCELE = 4;
+  const PAS_DE8MAGIE = 5;
+  const PERSO_ENDORMI = 6;
 
   //constructeur
   public function __construct(array $donnees){
     $this->hydrate($donnees);
-  }
-
-
-  public function frapper(Personnage $perso){
-    if($perso->id() == $this->_id){
-      return self::CEST_MOI;
-    }
-    return $perso->recevoirDegats();
+    $this->type = strtolower(get_class($this));
   }
 
   // HYDRATATION
@@ -33,52 +32,104 @@ class Personnage
     }
   }
 
-  public function recevoirDegats(){
-    $this->_degats += 5;
+  //METHODES
 
-    if($this->_degats >= 100){
+  public function estEndormi(){
+    return $this->timeEndormi > time();
+  }
+
+  public function frapper(Personnage $perso){
+    if($perso->id() == $this->id){
+      return self::CEST_MOI;
+    }
+    if ($this->estEndormi()){
+      return self::PERSO_ENDORMI;
+    }
+    return $perso->recevoirDegats();
+  }
+  
+  public function recevoirDegats(){
+    $this->degats += 5;
+
+    if($this->degats >= 100){
       return self::PERSONNAGE_TUE;
     }
     return self::PERSONNAGE_FRAPPE;
   }
 
+  public function reveil(){
+    $secondes = $this->timeEndormi;
+    $secondes -= time();
+
+    $heures   = floor($secondes / 3600);
+    $secondes -= $heures * 3600;
+    $minutes  = floor($secondes / 60);
+    $secondes -= $minutes * 60;
+
+    return $heures . 'h, ' . $minutes . 'min et ' . $secondes . 'sec';
+  }
+
+  public function nomValide(){
+    return !empty($this->nom);
+  }
+
 
   // GETTERS
+  public function atout(){
+    return $this->atout;
+  }
+
   public function id(){
-    return $this->_id;
+    return $this->id;
   }
 
   public function degats(){
-    return $this->_degats;
+    return $this->degats;
   }
 
   public function nom(){
-    return $this->_nom;
+    return $this->nom;
+  }
+
+  public function type(){
+    return $this->type;
+  }
+
+  public function timeEndormi(){
+    return $this->timeEndormi;
   }
 
   // SETTERS
+  public function setAtout($atout){
+    $atout = (int) $atout;
+    if ($atout >= 0 && $atout <= 100){
+      $this->atout = $atout;
+    }
+  }
+
   public function setId($id){
     $id = (int) $id;
     if ($id > 0){
-      $this->_id = $id;
+      $this->id = $id;
     }
   }
 
   public function setDegats($degats){
     $degats = (int) $degats;
     if ($degats >= 0 && $degats <= 100){
-      $this->_degats = $degats;
+      $this->degats = $degats;
     }
   }
 
   public function setNom($nom){
     if (is_string($nom)){
-      $this->_nom = $nom;
+      $this->nom = $nom;
     }
   }
   
-  public function nomValide(){
-    return !empty($this->_nom);
+  public function setTimeEndormi($time){
+    $this->timeEndormi = (int) $time;
   }
+
 }
 ?>
--- classes/PersonnagesManager.class.php
+++ classes/PersonnagesManager.class.php
@@ -10,33 +10,36 @@ class PersonnagesManager
  
   public function add(Personnage $perso)
   {
-    $q = $this->_db->prepare('INSERT INTO personnages SET nom = :nom');
+    $q = $this->_db->prepare('INSERT INTO personnages_v2 SET nom = :nom, type = :type');
  
     $q->bindValue(':nom', $perso->nom());
+    $q->bindValue(':type', $perso->type());
+
     $q->execute();
 
     $perso->hydrate(array(
       'id' => $this->_db->lastInsertId(),
       'degats' => 0,
+      'atout' => 0,
     ));
   }
   
   public function count(){
-    return $this->_db->query('SELECT COUNT(*) FROM personnages')->fetchColumn();
+    return $this->_db->query('SELECT COUNT(*) FROM personnages_v2')->fetchColumn();
   }
 
 
   public function delete(Personnage $perso){
-    $this->_db->exec('DELETE FROM personnages WHERE id = '.$perso->id());
+    $this->_db->exec('DELETE FROM personnages_v2 WHERE id = '.$perso->id());
   }
 
  
   public function exists($info){
     if(is_int($info)){
-      return (bool) $this->_db->query('SELECT COUNT(*) FROM personnages WHERE id =' . $info)->fetchColumn();
+      return (bool) $this->_db->query('SELECT COUNT(*) FROM personnages_v2 WHERE id =' . $info)->fetchColumn();
     }
 
-    $q = $this->_db->prepare('SELECT COUNT(*) FROM personnages WHERE nom = :nom');
+    $q = $this->_db->prepare('SELECT COUNT(*) FROM personnages_v2 WHERE nom = :nom');
     $q->execute(array(':nom' => $info));
 
     return (bool) $q->fetchColumn();
@@ -45,35 +48,44 @@ class PersonnagesManager
 
   public function get($info){
     if(is_int($info)){
-      $q = $this->_db->query('SELECT id, nom, degats FROM personnages WHERE id =' . $info);
-      $donnees = $q->fetch(PDO::FETCH_ASSOC);
-
-      return new Personnage($donnees);
+      $q = $this->_db->query('SELECT id, nom, degats, timeEndormi, type, atout FROM personnages_v2 WHERE id =' . $info);
+      $perso = $q->fetch(PDO::FETCH_ASSOC);
     }
     else {
-      $q = $this->_db->prepare('SELECT id, nom, degats FROM personnages WHERE nom = :nom');
+      $q = $this->_db->prepare('SELECT id, nom, degats, timeEndormi, type, atout FROM personnages_v2 WHERE nom = :nom');
       $q->execute(array(':nom' => $info));
+      $perso = $q->fetch(PDO::FETCH_ASSOC);
+    }
 
-      return new Personnage($q->fetch(PDO::FETCH_ASSOC));
+    switch ($perso['type']) {
+      case 'guerrier': return new Guerrier($perso);
+      case 'magicien': return new Magicien($perso);    
+      default: return null;
     }
   }
  
   public function getList($nom){
     $persos = array();
 
-    $q = $this->_db->prepare('SELECT id, nom, degats FROM personnages WHERE nom <> :nom ORDER BY nom');
+    $q = $this->_db->prepare('SELECT id, nom, degats, timeEndormi, type, atout FROM personnages_v2 WHERE nom <> :nom ORDER BY nom');
     $q->execute(array(':nom' => $nom));
 
     while ($donnees = $q->fetch(PDO::FETCH_ASSOC)) {
-      $persos[] = new Personnage($donnees);
+      switch ($donnees['type']) {
+        case 'guerrier': $persos[] = new Guerrier($donnees);
+          break;
+        case 'magicien': $persos[] = new Magicien($donnees);
+          break;
+      }
     }
-
     return $persos;
   }
  
   public function update(Personnage $perso){
-    $q = $this->_db->prepare('UPDATE personnages SET degats = :degats WHERE id = :id');
+    $q = $this->_db->prepare('UPDATE personnages_v2 SET degats = :degats, timeEndormi = :timeEndormi, atout = :atout  WHERE id = :id');
     $q->bindvalue(':degats', $perso->degats(), PDO::PARAM_INT);
+    $q->bindvalue(':timeEndormi', $perso->timeEndormi(), PDO::PARAM_INT);
+    $q->bindvalue(':atout', $perso->atout(), PDO::PARAM_INT);
     $q->bindvalue(':id', $perso->id(), PDO::PARAM_INT);
 
     $q->execute();
